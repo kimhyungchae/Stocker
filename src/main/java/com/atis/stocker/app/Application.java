949bbda974acc6c21d8b24e5b433de3a828455c3
kimhyungchae <kimhyungchae@gmail.com>
2016-06-05 22:01:38 +0900

    패키지변경

--- src/main/java/com/atis/stocker/app/Application.java
+++ src/main/java/com/atis/stocker/app/Application.java
@@ -2,10 +2,15 @@ package com.atis.stocker.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.orm.jpa.EntityScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@Configuration
+@EnableJpaRepositories(basePackages = "com.atis.*")
+@EntityScan(basePackages="com.atis.*")
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
